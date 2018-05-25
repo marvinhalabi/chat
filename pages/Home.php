dc975c3cbab0845ad2b87169df1e53ecd4fc28e2
Marvin Halabi <marvin.halabi@learnet.se>
2018-05-25 10:51:47 +0200

    väder implementerat

--- pages/Home.php
+++ pages/Home.php
@@ -41,6 +41,11 @@
                 $("#ChatMessages").load("DisplayMessages.php");
             });
 
+            window.onload = function() {
+                var objDiv = document.getElementById("ChatMessages");
+                objDiv.scrollTop = objDiv.scrollHeight;
+            }
+
         </script>
     </head>
 
@@ -48,11 +53,39 @@
         <h1>Welcome <span style="color:green"><?php echo $_SESSION['UserName']; ?></span></h1>
         <br>
 
-        <div id="ChatBig">
-            <div id="ChatMessages">
+
+
+        <?php
+	// Adress till vädertjänsten
+	$url = "http://api.openweathermap.org/data/2.5/weather?id=2673722&units=metric&APPID=22ee1d58f7adae08ee71fa7c0bd24481";
+
+	// Hämta data och avkoda JSON-formatet
+	$contents = file_get_contents($url);
+	$clima = json_decode($contents);
+
+	// Plocka ut temperaturer
+	$temp_max = $clima->main->temp_max;
+	$temp_min = $clima->main->temp_min;
+	$icon = $clima->weather[0]->icon.".png";
+
+	// Platsen och dagens datum
+	$today = date('d-m-Y H:i');
+	$city_name = $clima->name;
+
+	echo "<div class=\"ruta\">";
+	echo "<h3>$city_name</h3>";
+	echo "<h4>$today</h4>";
+	echo "<p>Temp max: $temp_max &deg;C</p>";
+	echo "<p>Temp min: $temp_min &deg;C</p>";
+	echo "<img src='http://openweathermap.org/img/w/$icon'>";
+	echo "</div>";
+	?>
+
+            <div id="ChatBig">
+                <div id="ChatMessages">
+                </div>
+                <textarea placeholder="Type your message..." id="ChatText" name="ChatText"></textarea>
             </div>
-            <textarea placeholder="Type your message..." id="ChatText" name="ChatText"></textarea>
-        </div>
 
     </body>
 
